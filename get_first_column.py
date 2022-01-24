b5eb5972897ceed1ab5743a1875a8b0d0621eea8
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2022-01-24 23:01:48 +0900

    4

--- get_first_column.py
+++ get_first_column.py
@@ -6,8 +6,16 @@ def get_first_column(data):
     Return:
         list: First column.
     """
+    
+    new_list=[]
     coloumn=data.split('\n')
-    coloumn_1=coloumn[1].split(',')
-    return coloumn_1
+    
+    
+    for idx in range(len(coloumn)):
+        coloumn2=coloumn[idx].split(',')
+        a=coloumn2[0]
+        new_list.append(a)
+
+    return new_list
     
 # Read the csv file
