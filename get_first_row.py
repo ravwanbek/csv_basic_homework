5c0efa2ce3ddbc1f6a85109329a8c0d101db2e01
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2022-01-24 21:12:55 +0900

    5

--- get_first_row.py
+++ get_first_row.py
@@ -1,12 +1,13 @@
 def get_first_row(data):   
-   """
-   Get the first row from a CSV file.
+    """
+    Get the first row from a CSV file.
     Args:
         data(str): csv file.
     Return:
         list: First row.
-   """
-   
-   return
-
+    """
+    row=data.split('\n')
+    row_1=row[1].split(',')
+    return row_1
+    
 # Read the csv file
