f6c2e6c16d57fb7762a8dee999bcaeaf2aa6e9d2
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2022-01-24 19:24:33 +0900

    rows

--- find_number_of_rows.py
+++ find_number_of_rows.py
@@ -1,3 +1,6 @@
+from itertools import count
+
+
 def find_number_of_rows(data):
     """
     Find the number of rows in CSV.
@@ -6,6 +9,15 @@ def find_number_of_rows(data):
     Return:
         int: Number of rows.
     """
-    return
+    
+    idx=0
+    coloumn=data.split('\n')
+    coloumn_len=coloumn[idx].split(',')
+    for x in coloumn:
+        idx+=1
+
+    return idx
+
+    
 
 # Read the csv file
